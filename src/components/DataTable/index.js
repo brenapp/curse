c1585a700cd7de994e7c16105cc44fb2cb48ae97
MayorMonty <artificalchamber@gmail.com>
2018-04-12 20:14:00 -0400

    DataTable

--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -1,6 +1,10 @@
 import { h, Component } from "preact";
 import Card from "preact-material-components/Card";
 import "preact-material-components/Card/style.css";
+import List from "preact-material-components/List";
+import "preact-material-components/List/style.css";
+import Checkbox from "preact-material-components/Checkbox";
+import "preact-material-components/Checkbox/style.css";
 import "preact-material-components/Button/style.css";
 
 import Label from "../Label";
@@ -12,7 +16,24 @@ export default class DataTable extends Component {
         selection: []
     }
 
-    render({ selectable, rows, data }) {
-
+    render({ selectable, cols, data }) {
+        return <div class={style.fullwidth}>
+            <List>
+                <List.Item class={style.header}>
+                    {selectable ?
+                        <Checkbox id="basic-checkbox" class={style.checkbox} />
+                        : null}
+                    {cols.map(col => <span>{col}</span>)}
+                </List.Item>
+                {data.map(item =>
+                    <List.Item class={style.main}>
+                        {selectable ?
+                            <Checkbox id="basic-checkbox" class={style.checkbox} />
+                            : null}
+                        {cols.map((col, i) => <span>{item[i]}</span>)}
+                    </List.Item>
+                )}
+            </List>
+        </div >
     }
 }
