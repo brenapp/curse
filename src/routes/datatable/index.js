7701e9e76c3c0acad72c580b59a8fbeb11d1cd83
MayorMonty <artificalchamber@gmail.com>
2018-04-12 20:14:16 -0400

    Update Test

--- src/routes/datatable/index.js
+++ src/routes/datatable/index.js
@@ -7,21 +7,17 @@ export default class DataTableTest extends Component {
     render() {
         return <div style={{ maxWidth: "760px", margin: "0 auto", padding: "32px" }}>
             <Card>
-                <div style={{ margin: "24px", marginTop: "0" }}>
-                    <h2 class=" mdc-typography--title">Hi</h2>
-                    <div class=" mdc-typography--caption">Let me subtitle</div>
-                </div>
                 <Card.Media className="card-media">
-                    <DataTable 
+                    <DataTable
                         selectable={true}
-                        rows={["SKU", "Name", "Teams", "Region", "Type"]}
-                        data={[...Array(10)].map(a => ["A", "B", "C", "D", "E"])}
+                        cols={["SKU", "Name", "Teams", "Region", "Type"]}
+                        data={Array(10).fill(["A", "B", "C", "D", "E"])}
                     />
                 </Card.Media>
                 <Card.Actions>
                     <Card.ActionButton>OKAY</Card.ActionButton>
                 </Card.Actions>
-                </Card>
-            </div>
+            </Card>
+        </div>
     }
 }
