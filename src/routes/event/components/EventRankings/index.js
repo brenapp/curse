d9cbcec4f0fa415222e44f8ac299cfb034d54643
MayorMonty <artificalchamber@gmail.com>
2018-04-13 18:51:11 -0400

    Use table for rankings

--- src/routes/event/components/EventRankings/index.js
+++ src/routes/event/components/EventRankings/index.js
@@ -3,6 +3,7 @@ import Card from "preact-material-components/Card";
 import "preact-material-components/Card/style.css";
 import List from "preact-material-components/List";
 import "preact-material-components/List/style.css";
+import DataTable from "../../../../components/DataTable";
 
 import style from "../../style";
 
@@ -24,27 +25,37 @@ export default class EventRankings extends Component {
         let { rankings, event, teamInfo } = data;
         console.log(this.state.division)
         return <Card class={style.marginTop}>
-            <div class={style.cardHeader}>
-                <strong>Rankings</strong>
-            </div>
-            <DivisionTabs
-                divisions={event.divisions}
-                onChange={
-                    div => this.setState({ division: event.divisions.indexOf(div) })
-                }
-            />
-            <List dense>
-                {
+            <DataTable
+                selectable
+                title={"Rankings"}
+                cols={["Rank", "Team", "Name", "W-L-T", "WP / AP / SP"]}
+                data={
                     rankings
-                        .filter(rank => rank.division == event.divisions[this.state.division])
-                        .map(rank => teamInfo[rank.team] ?
-                            <List.LinkItem href={`/team/${rank.team}`} ripple>
-                                <List.PrimaryText>{rank.rank}. {decodeHTML(teamInfo[rank.team].team_name)}</List.PrimaryText>
-                                <span class="mdc-list-item__meta">{rank.team}</span>
-                            </List.LinkItem> : undefined
+                        .filter(rank => rank.division == event.divisions[this.state.division] && teamInfo[rank.team])
+                        .map(rank => [
+                            rank.rank,
+                            rank.team,
+                            decodeHTML(teamInfo[rank.team].team_name),
+                            `${[rank.wins, rank.losses, rank.ties].join("-")}`,
+                            `${[rank.wp, rank.ap, rank.sp].join(" / ")}`]
                         )
                 }
-            </List>
+                actions={{
+                    compare() {
+                        alert("Compare")
+                    },
+                    assessment() {
+                        alert("Send to Analyzer")
+                    }
+                }}
+            >
+                <DivisionTabs
+                    divisions={event.divisions}
+                    onChange={
+                        div => this.setState({ division: event.divisions.indexOf(div) })
+                    }
+                />
+            </DataTable>
         </Card>
     }
 }
